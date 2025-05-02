9a395ac5020537df091858a756c31e009861a02a
legor0210 <nickorogel@gmail.com>
2025-05-03 00:44:05 +0800

    added cros browser setting

--- src/main/java/org/example/base/BaseTest.java
+++ src/main/java/org/example/base/BaseTest.java
@@ -3,6 +3,9 @@ package org.example.base;
 import com.aventstack.extentreports.ExtentTest;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.*;
 import org.example.utils.ExtentManager;
 import org.example.utils.ExtentTestManager;
@@ -20,11 +23,30 @@ public class BaseTest {
 
     @BeforeMethod
     public void setup(Method method) {
-        driver = new ChromeDriver();
+        String browser = System.getProperty("browser", "edge").toLowerCase();
+
+        // Initialize the test in ExtentReports before any logging
         ExtentTestManager.startTest(method.getName());
+
         Log.info("Starting test: " + method.getName());
 
+        switch (browser) {
+            case "firefox":
+                driver = new FirefoxDriver();
+                Log.info("Launching Firefox browser.");
+                break;
+            case "edge":
+                driver = new EdgeDriver();
+                Log.info("Launching Edge browser.");
+                break;
+            case "chrome":
+            default:
+                driver = new ChromeDriver();
+                Log.info("Launching Chrome browser.");
+                break;
+        }
     }
+
 
     @AfterMethod
     public void tearDown() {
